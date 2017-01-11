ec89ec7ae2bc9b62fbf121ba6f8af4015dccae1d
Chris Caruso <carusocr@ldc.upenn.edu>
2017-01-10 19:25:14 -0800

    c3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'zug/zugme'
+  get 'zug/zugyou'
   get 'static_pages/home'
-
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
 
-  root 'application#crc'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
