2914631e9947d18df6a983755135112306174086
Chris Caruso <carusocr@ldc.upenn.edu>
2017-01-28 17:06:02 -0800

    basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  include SessionsHelper
   def crc
     render html: "<h1></strong>crc</strong></h1>".html_safe
   end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,24 +2,30 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test "invalid signup" do
+  test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: {user: { name: '', email: 'zz', password: 'zug', password_confirmation: 'zuggo' } }
+      post signup_path, params: {user: { name: '', email: 'zz', password: 'zug', password_confirmation: 'zuggo' } }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
   end
 
-  test "valid signup" do
+  test "valid signup information" do
     get signup_path
-    assert_difference 'User.count' do
-      post users_path, params: {user: { name: 'Zorg', email: 'z@z.com', password: 'zug'*3, password_confirmation: 'zug'*3 } }
+    assert_template 'users/new'
+    assert_difference 'User.count', 1 do
+      post users_path, params: {user: { name: "Zorg", email: "zzz@zz.com", password: "zugzugzug", password_confirmation: "zugzugzug" } }
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.alert
+    assert is_logged_in?
+  end
+
+  test "user count" do
+    assert_equal 1, User.count
+    get signup_path
   end
     
   
